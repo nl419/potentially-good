026bcab4c7561822937e59710e284a271180d97a
Nik <nl419@cam.ac.uk>
2023-06-01 12:58:14 +0100

    uhh

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # potentially-good
 oh dear god my eyes are bleeding
+
+# report 
+## start brick, see result
+
+## start naca, tweak (done)
+
+## by hand
+
+## vectorisation
+
+## code listings?
+- Show the changes wrt previous reports
+
+## same again, but different speed
+
+## maybe consider how useful an aerofoil which stalls at 4 degrees is
+
+## reduce stall risk
+
+## bullet points from handout
+
+# Make my own NACA-style aerofoil series
--- week 3/even_worse.m
+++ week 3/even_worse.m
@@ -6,17 +6,37 @@ clear all
 
 Re = 0.5e6;
 np = 400;
-alpha = 0:1:20;
-id = '8505';
+alpha = 0:0.25:8;
+id = '0020';
 
 %% Literal hell
 [xk0, yk0] = naca4_nowrite(id);
+% [xk0, yk0] = textread("Geometry\opt.surf", '%f%f');
 
-uplim_y = 0.15*ones(length(yk0));
-lowlim_y = -0.05*ones(length(yk0));
+uplim_y = 0.05*ones([length(yk0),1]);
+lowlim_y = -0.05*ones([length(yk0),1]);
+uplim_y(1) = 0;
+uplim_y(end) = 0;
+lowlim_y(1) = 0;
+lowlim_y(end) = 0;
+uplim_y = uplim_y + yk0;
+lowlim_y = lowlim_y + yk0;
 
-fun = @foil_L_over_D_noid;
-y_fin = fmincon(@(y)foil_L_over_D_noid(xk0, y, np, alpha, Re),yk0,[],[],[],[],lowlim_y,uplim_y);
-max_LD = foil_L_over_D_noid(xk0, y_fin, np, alpha, Re);
+opts = optimoptions(@fmincon,'MaxFunctionEvaluations',500,'UseParallel',true);
 
-disp(-max_LD);
+y_fin = fmincon(@(y)foil_L_over_D_noid_negative(xk0, y, np, alpha, Re, true),yk0, ...
+    [],[],[],[],lowlim_y,uplim_y,[],opts);
+max_LD = -foil_L_over_D_noid_negative(xk0, y_fin, np, alpha, Re, false);
+
+disp(max_LD);
+disp(-foil_L_over_D_noid_negative(xk0, yk0, np, alpha, Re, false));
+plot(xk0, y_fin);
+hold on;
+plot(xk0, yk0);
+legend("New", "Old");
+
+fname = ['Geometry/opt_brick.surf'];
+
+fid = fopen(fname,'w');
+fprintf(fid, '%10.6f %10.6f \n', [xk0';y_fin']);
+fclose(fid);
